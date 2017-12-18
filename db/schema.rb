fdc169cc2f61b274a82111f1b9d6931c814d75a6
Kristen Kulha <kristen.m.kulha@gmail.com>
2017-12-18 13:39:12 -0800

    Add group form partialand description column to groups table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171218210258) do
+ActiveRecord::Schema.define(version: 20171218213724) do
 
   create_table "groups", force: :cascade do |t|
     t.string   "game_title"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20171218210258) do
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
     t.integer  "admin_id"
+    t.string   "description"
   end
 
   create_table "memberships", force: :cascade do |t|
