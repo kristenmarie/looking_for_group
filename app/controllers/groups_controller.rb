0deb02aa43a1d816e6ca1d89d1daacb126cd525e
Kristen Kulha <kristen.m.kulha@gmail.com>
2017-12-18 11:03:44 -0800

    Add group controller.

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,56 @@
+class GroupsController < ApplicationController
+
+  def home
+    @groups = groups.all
+  end
+
+  def index
+    @groups = groups.all
+  end
+
+  def show
+    @group = Group.find(params[:id])
+  end
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new(group_params)
+   if @group.save
+     flash[:notice] = "Your group has been created!"
+     redirect_to group_path(@group)
+   else
+     render :new
+   end
+  end
+
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  def update
+    @group = Group.find(params[:id])
+    if @group.update(group_params)
+      flash[:notice] = "Your #{@group.game_title} group has been updated!"
+      redirect_to product_path(@group)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @group = Group.find(params[:id])
+    @group.destroy
+    flash[:alert] = "Group has been deleted."
+    redirect_to products_path
+  end
+
+  private
+  
+  def group_params
+    params.require(:group).permit(:game_title, :players_needed, :location, :play_location)
+  end
+
+end
