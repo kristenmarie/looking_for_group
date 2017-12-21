d0499f06681ecbe2d8f363bf1e72a03528fccd47
Kristen Kulha <kristen.m.kulha@gmail.com>
2017-12-21 09:33:12 -0800

    Fix search bar alignment issues.

--- app/models/group.rb
+++ app/models/group.rb
@@ -6,4 +6,15 @@ class Group < ActiveRecord::Base
   def self.search(search)
     where("name LIKE ? OR game_title LIKE ? OR location LIKE ?", "%#{search}%", "%#{search}%", "%#{search}%")
   end
+
+  def self.not_full
+    groups = Group.all
+    not_full = []
+    groups.each do |group|
+      if group.players_needed < group.memberships.length
+        not_full.push(group)
+      end
+    end
+    not_full
+  end
 end
