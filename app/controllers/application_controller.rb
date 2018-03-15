e189ba0e77a3f02a550550cbb95935eed2f817d3
Kristen Kulha <kristen.m.kulha@gmail.com>
2018-03-15 14:50:34 -0700

    Make sure devise permits avatar.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,7 @@ class ApplicationController < ActionController::Base
 
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+      devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :password, :current_password, :avatar) }
     end
 
     def login_required
