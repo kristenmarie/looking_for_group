b05fee8017b124034e7271c7de20c1a5a79f21da
Kristen Kulha <kristen.m.kulha@gmail.com>
2018-03-15 14:47:06 -0700

    Declare attached file to user, add validation and medium/thumb images.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,7 @@ class User < ApplicationRecord
   has_many :memberships
   has_many :groups, through: :memberships
   validates :username, :presence => true
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100#" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
 end
