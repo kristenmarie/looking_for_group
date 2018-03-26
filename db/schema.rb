864683e3118ad270369c047f82fbb9e52b3223aa
Kristen Kulha <kristen.m.kulha@gmail.com>
2018-03-26 15:00:35 -0700

    Add bio to users table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180315213219) do
+ActiveRecord::Schema.define(version: 20180326215854) do
 
   create_table "groups", force: :cascade do |t|
     t.string   "game_title"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 20180315213219) do
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.string   "bio"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
